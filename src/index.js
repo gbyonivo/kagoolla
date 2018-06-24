be3817ddef007188db72afdf61723ab0e27bf4c2
gbyon <gbyonivo@gmail.com>
2018-06-24 19:44:21 +0100

    created grid of beer images

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ const carousel = createElement('div', { id: 'carousels' });
 const grid = createElement('div', { id: 'grids' });
 
 carousel.innerHTML = 'carousel';
-grid.innerHTML = 'grids';
 
 addElementToPage(accordion);
 addElementToPage(carousel);
